be6742712949407a5f117b31d0534fadbf7dac72
blurryfacebeat <blurryfacebeat@gmail.com>
2020-10-27 22:27:12 +0700

    Много сделал

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 "use strict";
 
 // Получаю данные из формы и обрабатываю их в JSON
-var $search_form = document.querySelector('.search-form__app');
-var $search_inputs = document.querySelectorAll('.search-form__app input');
-var searchResult = {
-  from: '',
-  to: '',
-  there: '',
-  back: '',
-  count: ''
-};
-$search_form.addEventListener('submit', function (e) {
-  searchResult.from = $search_inputs[0].value;
-  searchResult.to = $search_inputs[1].value;
-  searchResult.there = $search_inputs[2].value;
-  searchResult.back = $search_inputs[3].value;
-  searchResult.count = $search_inputs[4].value;
-  delete localStorage.searchResult;
-  localStorage.setItem('searchResult', JSON.stringify(searchResult));
-}); // Добавляю маску к логину и регистрации
-
-var $reg_phone_field = document.querySelector('.reg-phone');
-$reg_phone_field.addEventListener('click', setMask);
-
-function setMask(event) {
-  var matrix = this.getAttribute('data-mask');
-  matrix = matrix.replace(/9/g, '_');
-  var i = 0;
-  var def = matrix.replace(/\D/g, "");
-  var val = this.value.replace(/\D/g, "");
-
-  if (def.length >= val.length) {
-    val = def;
-  }
-
-  this.value = matrix.replace(/./g, function (a) {
-    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+try {
+  var $search_form = document.querySelector('.search-form__app');
+  var $search_inputs = document.querySelectorAll('.search-form__app input');
+  var searchResult = {
+    from: '',
+    to: '',
+    there: '',
+    back: '',
+    count: ''
+  };
+  $search_form.addEventListener('submit', function (e) {
+    searchResult.from = $search_inputs[0].value;
+    searchResult.to = $search_inputs[1].value;
+    searchResult.there = $search_inputs[2].value;
+    searchResult.back = $search_inputs[3].value;
+    searchResult.count = $search_inputs[4].value;
+    delete localStorage.searchResult;
+    localStorage.setItem('searchResult', JSON.stringify(searchResult));
   });
+} catch (error) {} // Добавляю маску к логину и регистрации
 
-  if (event.type == "blur") {
-    if (this.value.length === 2) {
-      this.value = "";
+
+try {
+  var setMask = function setMask(event) {
+    var matrix = this.getAttribute('data-mask');
+    matrix = matrix.replace(/9/g, '_');
+    var i = 0;
+    var def = matrix.replace(/\D/g, "");
+    var val = this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) {
+      val = def;
     }
-  }
-}
+
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+    });
+
+    if (event.type == "blur") {
+      if (this.value.length === 2) {
+        this.value = "";
+      }
+    }
+  };
+
+  var $reg_phone_field = document.querySelector('.reg-phone');
+  var $reg_passport = document.getElementById('doc-num');
+  $reg_phone_field.addEventListener('input', setMask);
+  $reg_phone_field.addEventListener('focus', setMask);
+  $reg_phone_field.addEventListener('blur', setMask);
+  $reg_passport.addEventListener('input', setMask);
+  $reg_passport.addEventListener('focus', setMask);
+  $reg_passport.addEventListener('blur', setMask);
+} catch (error) {} // Работаю со страницей поиска
+
+
+try {
+  var $info_results = document.querySelectorAll('.info-result');
+  var fligthsResult = JSON.parse(localStorage.getItem('searchResult'));
+  $info_results[0].innerHTML = fligthsResult.from;
+  $info_results[1].innerHTML = fligthsResult.to;
+  $info_results[2].innerHTML = fligthsResult.there;
+  $info_results[3].innerHTML = fligthsResult.back;
+  $info_results[4].innerHTML = fligthsResult.count;
+} catch (error) {}
--- js/index.js
+++ js/index.js
@@ -1,47 +1,74 @@
 // Получаю данные из формы и обрабатываю их в JSON
-const $search_form = document.querySelector('.search-form__app');
-const $search_inputs = document.querySelectorAll('.search-form__app input');
-const searchResult = {
-    from: '',
-    to: '',
-    there: '',
-    back: '',
-    count: ''
-};
+try {
+    const $search_form = document.querySelector('.search-form__app');
+    const $search_inputs = document.querySelectorAll('.search-form__app input');
+    const searchResult = {
+        from: '',
+        to: '',
+        there: '',
+        back: '',
+        count: ''
+    };
 
-$search_form.addEventListener('submit', function(e) {
-    searchResult.from = $search_inputs[0].value;
-    searchResult.to = $search_inputs[1].value;
-    searchResult.there = $search_inputs[2].value;
-    searchResult.back = $search_inputs[3].value;
-    searchResult.count = $search_inputs[4].value;
+    $search_form.addEventListener('submit', function(e) {
+        searchResult.from = $search_inputs[0].value;
+        searchResult.to = $search_inputs[1].value;
+        searchResult.there = $search_inputs[2].value;
+        searchResult.back = $search_inputs[3].value;
+        searchResult.count = $search_inputs[4].value;
 
-    delete localStorage.searchResult;
-    localStorage.setItem('searchResult', JSON.stringify(searchResult));
-});
+        delete localStorage.searchResult;
+        localStorage.setItem('searchResult', JSON.stringify(searchResult));
+    });
+} catch (error) {
+    
+}
 
 // Добавляю маску к логину и регистрации
-const $reg_phone_field = document.querySelector('.reg-phone');
-$reg_phone_field.addEventListener('click', setMask);
+try {
+    const $reg_phone_field = document.querySelector('.reg-phone');
+    const $reg_passport = document.getElementById('doc-num');
+    $reg_phone_field.addEventListener('input', setMask);
+    $reg_phone_field.addEventListener('focus', setMask);
+    $reg_phone_field.addEventListener('blur', setMask);
+    $reg_passport.addEventListener('input', setMask);
+    $reg_passport.addEventListener('focus', setMask);
+    $reg_passport.addEventListener('blur', setMask);
 
-function setMask(event) {
-    let matrix = this.getAttribute('data-mask');
-    matrix = matrix.replace(/9/g, '_');
-    let i = 0;
-    let def = matrix.replace(/\D/g, "");
-    let val = this.value.replace(/\D/g, "");
+    function setMask(event) {
+        let matrix = this.getAttribute('data-mask');
+        matrix = matrix.replace(/9/g, '_');
+        let i = 0;
+        let def = matrix.replace(/\D/g, "");
+        let val = this.value.replace(/\D/g, "");
 
-    if (def.length >= val.length) {
-        val = def;
-    }
-    
-    this.value = matrix.replace(/./g, function(a) {
-        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
-    });
-
-    if (event.type == "blur") {
-        if (this.value.length === 2) {
-            this.value = "";
+        if (def.length >= val.length) {
+            val = def;
         }
-    }
+        
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+        });
+
+        if (event.type == "blur") {
+            if (this.value.length === 2) {
+                this.value = "";
+            }
+        }
+}
+} catch (error) {
+
+}
+
+// Работаю со страницей поиска
+try {
+    const $info_results = document.querySelectorAll('.info-result');
+    const fligthsResult = JSON.parse(localStorage.getItem('searchResult'));
+    $info_results[0].innerHTML = fligthsResult.from;
+    $info_results[1].innerHTML = fligthsResult.to;
+    $info_results[2].innerHTML = fligthsResult.there;
+    $info_results[3].innerHTML = fligthsResult.back;
+    $info_results[4].innerHTML = fligthsResult.count;
+} catch (error) {
+
 }
